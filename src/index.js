f5a594800df26dec57c6b69ec5fdb078db28d9ea
Unn2007 <unn2007@i.ua>
2023-05-07 00:02:17 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const refs = { bestsellerContainer: document.querySelector(".bestseller-container"), }
+
 const fetchTopBooks = async () => {
   const response = await fetch(
     'https://books-backend.p.goit.global/books/top-books'
@@ -9,24 +11,33 @@ const fetchTopBooks = async () => {
 const getListTopBooks = async () => {
   try {
     const topBooks = await fetchTopBooks();
-    let markup = `<h1 class="bestseller-header">
-        Best Sellers <span class="bestseller-header-part">Books</span>
-      </h1>
-      <div class="bestseller-category">`;
-    topBooks.map(({ books, list_name }, i) => {
-      if (i < 3) {
-        
-        markup =
-          markup + `<h3 class="bestseller-category-header">${list_name}</h3>`;
-        // console.log(`_____________________${i}____________________`,markup)
+    renderMarkupBestseller(topBooks)
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-        books.map(({ _id, author, book_image, title }, index) => {
-          if (index === 0) {
-            markup = markup + `<ul class="bestseller-category-card-set">`;
-          }
-          markup =
-            markup +
-            `               
+function renderMarkupBestseller(topBooks) {
+  let markup = `<h1 class="bestseller-header">
+        Best Sellers <span class="bestseller-header-part">Books</span>
+      </h1>`;
+  topBooks.map(({ books, list_name }, i) => {
+    if (i < 4) {
+
+      markup =
+        markup + `
+          <div class="bestseller-category">
+          <h3 class="bestseller-category-header">${list_name}</h3>`;
+
+
+      books.map(({ _id, author, book_image, title }, index) => {
+        if (index === 0) {
+          markup = markup + `
+             <ul class="bestseller-category-card-set">`;
+        }
+        markup =
+          markup +
+          `               
             <li class="card-set-item" id="${_id}">
               <a href="" class="bestseller-card-link">
                 <article class="bestseller-card">
@@ -44,32 +55,23 @@ const getListTopBooks = async () => {
                 </article>
               </a>
             </li>`;
-          if (index === 4) {
-            markup =
-              markup +
-              `</ul>
+        if (index === 4) {
+          markup =
+            markup +
+            `</ul>
                      <div class="bestseller-button-container" >
                         <a href="" class="bestseller-see-more" id="${_id}">see more</a>
           
                       </div>
           
                     </div>`;
-          }
-          // console.log("__________________________________",markup)
-        });
+        }
 
-        // if (i===3) {
-        //   markup =  markup +`</div>`
-        //   return
-        // }
-      }
-    });
-    // console.log(markup)
+      });
 
-    console.log(markup);
-  } catch (error) {
-    console.log(error.message);
-  }
-};
 
+    }
+  });
+  refs.bestsellerContainer.innerHTML = markup
+}
 getListTopBooks();
