23414c2fd93c716a2b86ee89e765dfcbf1d0798c
Aleks-TO <satov69@gmail.com>
2023-05-06 21:13:18 +0300

    axios instal

--- src/JS/fetch-api/fetch-api.js
+++ src/JS/fetch-api/fetch-api.js
@@ -3,4 +3,10 @@ import axios from 'axios';
 class ApiBook {
   API_URL = 'https://books-backend.p.goit.global/books';
   constructor() {}
+  async getCategoryList() {
+    const { data } = await axios.get(`${this.API_URL}/category-list `);
+    return data;
+  }
 }
+const api = new ApiBook();
+console.log(api.getCategoryList());
