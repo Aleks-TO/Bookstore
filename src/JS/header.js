b2b6bba64980ad8ab8da4e5957d76ccdecf95a8f
gitdenlev <levchenkoden02@gmail.com>
2023-05-08 00:42:12 +0200

    Fix bugs

--- src/JS/header.js
+++ src/JS/header.js
@@ -1,8 +1,15 @@
-const burgerMenu = document.querySelector('.burger-menu'); // Бургер-меню
-
-const modalOverlay = document.querySelector('.modal-overlay'); // Модальне-вікно
+const burgerMenu = document.querySelector('.burger-menu');
+const modalOverlay = document.querySelector('.modal-overlay');
 
 burgerMenu.addEventListener('click', function () {
-  // Прослуховувач події на закриття і відкриття модального вікна
   modalOverlay.classList.toggle('show');
 });
+
+
+
+
+
+
+
+
+
