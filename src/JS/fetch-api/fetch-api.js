9b61ec3bc08986e71fa7e163f16d532647159508
Aleks-TO <satov69@gmail.com>
2023-05-07 10:32:34 +0300

    add api book class

--- src/JS/fetch-api/fetch-api.js
+++ src/JS/fetch-api/fetch-api.js
@@ -1,12 +1,20 @@
 import axios from 'axios';
 
-class ApiBook {
+export default class ApiBook {
   API_URL = 'https://books-backend.p.goit.global/books';
   constructor() {}
   async getCategoryList() {
-    const { data } = await axios.get(`${this.API_URL}/category-list `);
-    return data;
+    return (await axios.get(`${this.API_URL}/category-list`)).data;
+  }
+  async getTopBooks() {
+    return (await axios.get(`${this.API_URL}/top-books`)).data;
+  }
+  async getBooksOfCategory(selectedCategory) {
+    return (
+      await axios.get(`${this.API_URL}/category?category=${selectedCategory}`)
+    ).data;
+  }
+  async getBookById(bookId) {
+    return (await axios.get(`${this.API_URL}/${bookId}`)).data;
   }
 }
-const api = new ApiBook();
-console.log(api.getCategoryList());
