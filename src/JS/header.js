68b9689511f09f84de4268479d38c204ca94f42f
gitdenlev <levchenkoden02@gmail.com>
2023-05-07 22:09:42 +0200

    End header section

--- src/JS/header.js
+++ src/JS/header.js
@@ -1,2 +1,8 @@
+const burgerMenu = document.querySelector('.burger-menu'); // Бургер-меню
 
+const modalOverlay = document.querySelector('.modal-overlay'); // Модальне-вікно
 
+burgerMenu.addEventListener('click', function () {
+  // Прослуховувач події на закриття і відкриття модального вікна
+  modalOverlay.classList.toggle('show');
+});
