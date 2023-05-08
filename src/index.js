a0b992371fa12dd72e9acce25be8d299a7d63912
Dima-kzk <dimakzk@yahoo.com>
2023-05-08 22:35:01 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+import './JS/header.js';
+import './JS/sidebar.js';
 import './JS/bestseller.js';
+import './JS/fetch-api/fetch-api.js';
+import './JS/support_Ukraine.js';
+
+import './JS/color-theme.js';
+
 import './JS/bookModal.js';
