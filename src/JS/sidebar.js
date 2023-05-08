19efbdaaa386f9eca71127ff9d8080d9a63f6670
Promart1 <marta.batih@gmail.com>
2023-05-08 17:48:42 +0300

    upd

--- src/JS/sidebar.js
+++ src/JS/sidebar.js
@@ -1,23 +1,27 @@
 const categoryList = document.querySelector('.siteBar-category-list');
+const allCategoryBTN = document.getElementById('data-category-id');
+ 
+// allCategoryBTN.addEventListener('click', onClickBestseller);
 
-
-
-
-     fetch(`https://books-backend.p.goit.global/books/category-list`)
-    .then(response => response.json()).then((books) => {
-        const makeupListCategory = books.map((book) => {
-            return renderBookList(book);
-        }).join('');
-        console.log(books);
-        categoryList.insertAdjacentHTML("beforeend", makeupListCategory);
-    });
-          
-
-
-
-function renderBookList({list_name}) {
-    
-        return `<li class="siteBar-category-item">${list_name}</li>`;
-
-    
-}
+async function fetchBooks() {
+    try {
+      const response = await fetch("https://books-backend.p.goit.global/books/category-list");
+      const books = await response.json();
+      
+      const makeupListCategory = books.map((book) => {
+        return renderBookList(book);
+      }).join('');
+      
+      console.log(books);
+      categoryList.insertAdjacentHTML("beforeend", makeupListCategory);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  function renderBookList({ list_name }) {
+    return `<li class="siteBar-category-item">${list_name}</li>`;
+  }
+  
+  fetchBooks();
+  
