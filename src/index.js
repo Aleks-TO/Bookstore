3fa9d9fe4ea8028fba2c8f3e6faa6f3468a0201a
Dima-kzk <dimakzk@yahoo.com>
2023-05-08 22:53:35 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import './JS/fetch-api/fetch-api.js';
 import './JS/header.js';
 import './JS/sidebar.js';
 import './JS/bestseller.js';
-import './JS/fetch-api/fetch-api.js';
+
 import './JS/support_Ukraine.js';
 
 import './JS/color-theme.js';
