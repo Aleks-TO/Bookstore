c68cbb66de957a077c04f6e4951282b8bd9bac8b
Dana <dana.lalovaod@gmail.com>
2023-05-10 21:56:43 +0300

    fix: re-work white images for dark mode

--- src/JS/color-theme.js
+++ src/JS/color-theme.js
@@ -1,55 +1,70 @@
+import closeBtnWhite from '../images/icons-img/x-close-white.png';
+import logoWhite from '../images/icons-img/logo-white.png';
+import amazonLogoWhite from '../images/icons-img/amazon-white.png';
+
+import closeBtnDark from '../images/bookModal/Icon_Close_24.png';
+import logoDark from '../images/icons-img/logo.png';
+import amazonLogoDark from '../images/icons-img/amazon.png';
+
 const refs = {
   theme: document.querySelector('#toggler-1'),
   body: document.querySelector('body'),
-  logo: document.querySelector('.logo'),
+  logo: document.querySelector('#logo'),
+  closeBtn: document.querySelector('#close-btn'),
+  amazonIcon: document.querySelector('.imgIcon'),
 };
 
 let isDarkTheme = false;
 
-function isThemeSaved (){
-    try {
-        let currentTheme = localStorage.getItem('isDarkTheme');
-        console.log('currentTheme', currentTheme);
-        if (currentTheme === null) {
-          return;
-        } if (currentTheme === 'true'){
-          isDarkTheme = true;
-          document.body.classList.add('dark-theme');
-          refs.theme.setAttribute('checked', 'true');
-          refs.logo.firstElementChild.src = `logo-white.17fec2eb.png`;
+function isThemeSaved() {
+  try {
+    let currentTheme = localStorage.getItem('isDarkTheme');
+    if (currentTheme === null) {
+      return;
+    }
+    if (currentTheme === 'true') {
+      isDarkTheme = true;
+      document.body.classList.add('dark-theme');
+      refs.theme.setAttribute('checked', 'true');
 
-      
-        }
-      } catch (error) {
-        console.log('error for dark theme switcher', error);
-      }
+      // білі іконки
+      refs.closeBtn.src = `${closeBtnWhite}`;
+      refs.logo.src = `${logoWhite}`;
+      refs.amazonIcon.src = `${amazonLogoWhite}`;
+    }
+  } catch (error) {
+    console.log('error for dark theme switcher', error);
+  }
 }
 
-isThemeSaved ();
-
-
-console.log(refs.theme);
+isThemeSaved();
 
 refs.theme.addEventListener('change', updateTheme);
 
 function updateTheme(e) {
-
-    // isThemeSaved ();
-
-  console.log('click');
   e.preventDefault();
   localStorage.removeItem('isDarkTheme');
 
   if (refs.theme.checked === true) {
     isDarkTheme = true;
     document.body.classList.add('dark-theme');
-    console.log(refs.logo.firstElementChild);
-    refs.logo.firstElementChild.src = `logo-white.17fec2eb.png`;
+
+    // білі іконки на темну тему
+    refs.closeBtn.src = `${closeBtnWhite}`;
+    refs.logo.src = `${logoWhite}`;
+    refs.amazonIcon.src = `${amazonLogoWhite}`;
+
   } else {
     isDarkTheme = false;
     document.body.classList.remove('dark-theme');
-    refs.logo.firstElementChild.src = `/logo.024aa838.png`;
-    //src/images/icons-img/logo-white.png
+    
+    // темні іконки
+    refs.closeBtn.src = `${closeBtnDark}`;
+    refs.logo.src = `${logoDark}`;
+    refs.amazonIcon.src = `${amazonLogoDark}`;
+
+
   }
+  console.log(('isDarkTheme: ', `${isDarkTheme}`));
   localStorage.setItem('isDarkTheme', `${isDarkTheme}`);
 }
