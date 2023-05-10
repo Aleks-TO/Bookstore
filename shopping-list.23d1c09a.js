c88d3d4fb1efa884e276050975327590955cd64c
Aleks-TO <114433644+Aleks-TO@users.noreply.github.com>
2023-05-10 18:27:03 +0000

    Deploying to gh-pages from @ Aleks-TO/Bookstore@888a83cd13a5f6c50e581a8f7c93153d7d8f5d96 🚀

---
+++ shopping-list.23d1c09a.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".burger-menu"),t=document.querySelector(".modal-overlay");e.addEventListener("click",(function(){t.classList.toggle("show")}));var n=document.querySelector(".shoplist-btn");n.addEventListener("click",(function(e){window.location.href="./shopping-list.html",n.classList.add("current")})),document.querySelector(".home-btn").addEventListener("click",(function(e){window.location.href="./index.html"}))}();
+//# sourceMappingURL=shopping-list.23d1c09a.js.map
---
+++ shopping-list.9761b070.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".burger-menu"),t=document.querySelector(".modal-overlay");e.addEventListener("click",(function(){t.classList.toggle("show")}));const n=document.querySelector(".shoplist-btn");n.addEventListener("click",(e=>{window.location.href="./shopping-list.html",n.classList.add("current")}));document.querySelector(".home-btn").addEventListener("click",(e=>{window.location.href="./index.html"}));
+//# sourceMappingURL=shopping-list.9761b070.js.map
