91311ace34768afeb8928246b4d7de4b80feafd7
Unn2007 <118598200+Unn2007@users.noreply.github.com>
2023-05-12 09:23:18 +0300

    upd

--- src/JS/header.js
+++ src/JS/header.js
@@ -1,9 +1,11 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const modalOverlay = document.querySelector('.modal-overlay');
+const closeIcon = document.querySelector('.close-icon');
 
 burgerMenu.addEventListener('click', function () {
   modalOverlay.classList.toggle('show');
 });
+
 
 const shoplistBtn = document.querySelector('.shoplist-btn');
 shoplistBtn.addEventListener('click', e => {
@@ -17,3 +19,36 @@ homeBtn.addEventListener('click', e => {
   // e.preventDefault();
   window.location.href = './index.html';
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const burgerMenu = document.querySelector('.burger-menu');
+// const modalOverlay = document.querySelector('.modal-overlay');
+
+// burgerMenu.addEventListener('click', function () {
+//   modalOverlay.classList.toggle('show');
+// });
+
+// const shoplistBtn = document.querySelector('.shoplist-btn');
+// shoplistBtn.addEventListener('click', e => {
+  // e.preventDefault();
+//   window.location.href = './shopping-list.html';
+//   shoplistBtn.classList.add('current');
+// });
+
+// const homeBtn = document.querySelector('.home-btn');
+// homeBtn.addEventListener('click', e => {
+  // e.preventDefault();
+//   window.location.href = './index.html';
+// });
