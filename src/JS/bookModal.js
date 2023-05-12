7cf2915afb50ad83d036376d982162728813623e
Dima-kzk <dimakzk@yahoo.com>
2023-05-12 16:46:32 +0300

    done

--- src/JS/bookModal.js
+++ src/JS/bookModal.js
@@ -9,10 +9,25 @@ const shoppingBtn = document.getElementById('shoppingBtn');
 const closeBtn = document.querySelector('.closeBtn');
 const icon = document.querySelector('#bookModal .shops-icons');
 
-// console.log(icon.children[0].children[0].href);
+bookModal.KEY_SHOP_LIST = 'BOOKSTORE_SHOP_LIST';
+bookModal.shopList = {};
 
-// icon.children[1].style.display = 'none';
-// icon.children[2].style.display = 'none';
+bookModal.has = function (id) {
+  let f = false;
+
+  if (localStorage.getItem(bookModal.KEY_SHOP_LIST) === null) {
+    return false;
+  }
+
+  bookModal.shopList = JSON.parse(
+    localStorage.getItem(bookModal.KEY_SHOP_LIST)
+  );
+  Object.keys(bookModal.shopList).forEach(k => {
+    if (k === id) f = true;
+  });
+
+  return f;
+};
 
 bookModal.hangLinks = function (book) {
   icon.children[0].children[0].href = book.buy_links[0].url;
